d7c536308d03d0743d4616c185adb65c637d38ed
bbs <cjswoqjatn1@naver.com>
2025-02-17 22:13:50 +0900

    메인화면 v1.5.0

---
+++ src/main/java/kr/co/mbc/dto/UserResponse.java
@@ -0,0 +1,29 @@
+package kr.co.mbc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class UserResponse {
+	private String username; // 유저네임
+	
+	private String password; // 패스워드
+	
+	private String name; // 네임
+
+	private String email; // 이메일
+
+	private String phone; // 전화번호
+	
+	private String role; // 등급
+
+}
--- src/main/java/kr/co/mbc/service/UserService.java
+++ src/main/java/kr/co/mbc/service/UserService.java
@@ -1,32 +1,5 @@
 package kr.co.mbc.service;
 
-import org.springframework.stereotype.Service;
-
-import kr.co.mbc.dto.UserForm;
-import kr.co.mbc.mapper.UserMapper;
-import kr.co.mbc.vo.UserVo;
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
 public class UserService {
-	
-	private final UserMapper userMapper;
-
-	public void save(UserForm userForm) {
-		
-		UserVo userVo = UserVo.builder()
-				.username(userForm.getUsername())
-				.password(userForm.getPassword())
-				.name(userForm.getName())
-				.email(userForm.getEmail())
-				.phone(userForm.getPhone())
-				.role("ROLE_USER").build();
-		userMapper.save(userVo);
-	}
-
-	public UserVo findByUsername(String username) {
-		return userMapper.findByUsername(username);
-	}
 
 }
