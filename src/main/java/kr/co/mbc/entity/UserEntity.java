8a35b5d27e4fbefa2ef8af49b510cb90459f6199
bbs <cjswoqjatn1@naver.com>
2025-02-18 11:36:12 +0900

    유저 인서트/리스트

--- src/main/java/kr/co/mbc/entity/UserEntity.java
+++ src/main/java/kr/co/mbc/entity/UserEntity.java
@@ -1,5 +1,11 @@
 package kr.co.mbc.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import kr.co.mbc.dto.UserForm;
 import kr.co.mbc.dto.UserResponse;
 import lombok.AllArgsConstructor;
@@ -8,7 +14,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
+@Entity
+@Table(name = "tbl_user")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -16,20 +23,43 @@ import lombok.ToString;
 @Builder
 @ToString
 public class UserEntity {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id; // 유저넘버
-
-	private String username; // 유저네임
-
+	@Column(nullable = false, unique = true)
+	private String username; // 아이디
+	@Column(nullable = false)
 	private String password; // 패스워드
-
+	@Column(nullable = false)
 	private String name; // 네임
-
 	private String email; // 이메일
-
 	private String phone; // 전화번호
 	
 	private String role; // 등급
 
+	
+	// userForm -> userEntity로 변환
+	public static UserEntity toUserEntity(UserForm userForm) {
+		return UserEntity.builder()
+				.username(userForm.getUsername())
+				.password(userForm.getPassword())
+				.name(userForm.getName())
+				.email(userForm.getEmail())
+				.phone(userForm.getPhone())
+				.role("ROLE_USER")
+				.build();
+	}
+	
+	// userEntity -> userResponse로 변환
+	public static UserResponse toUserResponse(UserEntity userEntity) {
+		return UserResponse.builder()
+				.username(userEntity.getUsername())
+				.name(userEntity.getName())
+				.password(userEntity.getPassword())
+				.email(userEntity.getEmail())
+				.phone(userEntity.getPhone())
+				.role(userEntity.getRole())
+				.build();
+	}
 	
 }
--- src/main/java/kr/co/mbc/service/UserService.java
+++ src/main/java/kr/co/mbc/service/UserService.java
@@ -1,5 +1,34 @@
 package kr.co.mbc.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.mbc.entity.UserEntity;
+import kr.co.mbc.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+	private final UserRepository userRepository;
+	
+
+	public UserEntity findByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+
+
+	public void save(UserEntity userEntity) {
+		userRepository.save(userEntity);
+	}
+
+
+	public List<UserEntity> findAll() {
+		List<UserEntity> list = userRepository.findAll();
+		return list;
+	}
+
+
 
 }
